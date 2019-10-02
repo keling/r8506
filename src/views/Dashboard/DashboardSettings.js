68945a2e0cd48a54b1cef28f8b4b00f15dd0ed07
Juster.D.Dragon <justic.dg@gmail.com>
2019-10-02 09:40:35 +0700

    edit dashboard key

--- src/views/Dashboard/DashboardSettings.js
+++ src/views/Dashboard/DashboardSettings.js
@@ -1,4 +1,4 @@
 module.exports = {
-    ACCESS_TOKEN: `cMhqtcyDfiwnnG9s3ZVfDkxoEcf34tnap4FZzd0zZErAcFo1tRhokPuRkO864DR5`,
+    ACCESS_TOKEN: `Pcv324GhCRy52w5Q572yQX1QZrhD3ySsZOEj4qPNU3oalRM1N4e3ubAGkqbaVWf2`,
     URL_API: `http://203.157.168.91:3000/api/`
 }
