2be4424bf18a7e558a6f01178b49760fdf08493f
memberren <pologotgame@gmail.com>
2019-12-26 16:31:04 +0700

    แก้ไขฟอร์มรายงานความทันเวลา

--- src/views/Report/Srrt/SrrtForm/SrrtForm.js
+++ src/views/Report/Srrt/SrrtForm/SrrtForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import axios from 'axios';
+import axios from 'axios'
 import DatePicker, { registerLocale } from 'react-datepicker'
 import th from 'date-fns/locale/th'
 import Select from 'react-select'
